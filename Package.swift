b885b5db7c08453fcb7a384c7cc451aab427639e
Juraj Ďurech <durech.juraj@gmail.com>
2021-10-18 12:23:20 +0200

    Deployment: Update release files to 0.0.2

--- Package.swift
+++ Package.swift
@@ -8,23 +8,17 @@ let package = Package(
         .tvOS(.v9)
     ],
     products: [
-        .library(name: "PowerAuth2", targets: ["PowerAuth2Wrapper"]),
-        .library(name: "PowerAuthCore", targets: ["PowerAuthCoreBinaryTarget"])
+        .library(name: "PowerAuth2", targets: ["PowerAuth2"]),
+        .library(name: "PowerAuthCore", targets: ["PowerAuthCore"])
     ],
     targets: [
-        .target(
-            name: "PowerAuth2Wrapper",
-            dependencies: [
-                .target(name: "PowerAuth2BinaryTarget"),
-                .target(name: "PowerAuthCoreBinaryTarget")
-            ]),
         .binaryTarget(
-            name: "PowerAuth2BinaryTarget",
-            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/0.0.1/PowerAuth2-1.6.2.xcframework.zip",
+            name: "PowerAuth2",
+            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/0.0.2/PowerAuth2-1.6.2.xcframework.zip",
             checksum: "bed5442992e31036128676bde73a5a75059cdb8287a68bd376ff39598bbb67b4"),
         .binaryTarget(
-            name: "PowerAuthCoreBinaryTarget",
-            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/0.0.1/PowerAuthCore-1.6.2.xcframework.zip",
-            checksum: "9874dd4c051265fdcbd6d3ddc121703cce9ced72b9b6cf914a00bda8b6edfbad"),
+            name: "PowerAuthCore",
+            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/0.0.2/PowerAuthCore-1.6.2.xcframework.zip",
+            checksum: "9874dd4c051265fdcbd6d3ddc121703cce9ced72b9b6cf914a00bda8b6edfbad")
     ]
 )
