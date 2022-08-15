34043c100c398fd765dd0fc7829ca5419e6adacd
Juraj Ďurech <durech.juraj@gmail.com>
2022-08-15 17:35:41 +0200

    Deployment: Update release files to 1.7.1

--- Package.swift
+++ Package.swift
@@ -14,11 +14,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PowerAuth2",
-            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/1.6.5/PowerAuth2-1.6.5.xcframework.zip",
-            checksum: "e59ef5afa1087a590890fa4ea58bedbca0b7b2416164f1efa569d30dd26ed238"),
+            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/1.7.1/PowerAuth2-1.7.1.xcframework.zip",
+            checksum: "75a1a03dece8881371596915935526c0eca3fa88cc74a3e8e63f6d63868818f6"),
         .binaryTarget(
             name: "PowerAuthCore",
-            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/1.6.5/PowerAuthCore-1.6.5.xcframework.zip",
-            checksum: "230fe7ad2e4f1580dc6f35e92275536b06348cf7f204d6d2039e53101edd162f")
+            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/1.7.1/PowerAuthCore-1.7.1.xcframework.zip",
+            checksum: "885f1b693f1e5ba67a56992e837e411e61594280c3f0542cad9a16ee22e01096")
     ]
 )
