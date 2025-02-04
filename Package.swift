663b9f59eb42bdb2d3ad2d3395f64a3fefc54109
Juraj Ďurech <durech.juraj@gmail.com>
2025-02-04 14:06:32 +0100

    Deployment: Update release files to 1.9.3

--- Package.swift
+++ Package.swift
@@ -14,11 +14,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PowerAuth2",
-            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/1.8.4/PowerAuth2-1.8.4.xcframework.zip",
-            checksum: "f4f87cbe07aeb5773651f3cb94438b5072ced800ccccc83c9d080b43753c700f"),
+            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/1.9.3/PowerAuth2-1.9.3.xcframework.zip",
+            checksum: "bd9195dbe6baf2525767047e560a871555f8cdcc7bb3a6f99fac279eae50c568"),
         .binaryTarget(
             name: "PowerAuthCore",
-            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/1.8.4/PowerAuthCore-1.8.4.xcframework.zip",
-            checksum: "70660b8f17305dacaa36acd00fefe01853e1ed238e634d8134f2c2eefeb0aadb")
+            url: "https://github.com/wultra/powerauth-mobile-sdk-spm/releases/download/1.9.3/PowerAuthCore-1.9.3.xcframework.zip",
+            checksum: "7dd13bde1cfce8b149d9218ac432a2d79041c8dedf22a451aa051e92f90acf4a")
     ]
 )
